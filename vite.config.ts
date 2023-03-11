f186118e6e3160a6c0a53c938a5c5620c16bb315
chuhemiao <team@nnsdao.com>
2023-03-11 15:05:46 +0400

    feat: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
-import ViteRsw from 'vite-plugin-rsw';
 
 export default defineConfig({
   plugins: [
-    reactRefresh(),
-    ViteRsw({
-      crates: [
-        'wasm-test',
-      ]
-    }),
-  ],
-  build: {
-    outDir: "/usr/local/share/project/docker-compose/nginx/html/nnsdao"
-  }
-})
+    reactRefresh()
+    // ViteRsw({
+    //   crates: [
+    //     'wasm-test',
+    //   ]
+    // }),
+  ]
+  // build: {
+  //   outDir: '/usr/local/share/project/docker-compose/nginx/html/nnsdao'
+  // }
+});
